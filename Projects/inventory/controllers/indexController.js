bac960d54c6b55ca700f171b632f33c1e86132e6
Danielle Niestadt <137403706+AltheaVestrit@users.noreply.github.com>
2026-08-21 17:30:21 +0200

    Project: inventory - render books from db on home page

--- Projects/inventory/controllers/indexController.js
+++ Projects/inventory/controllers/indexController.js
@@ -1,3 +1,7 @@
+const queries = require('../models/queries');
+
 exports.indexGet = async (req, res) => {
-    res.render('index');
+    const books = await queries.getAllBooks();
+    // const books = [{ title: 'book1', author: 'author1', genre: 'genre1' }, { title: 'book2', author: 'author2', genre: 'genre2' }, { title: 'book3', author: 'author3', genre: 'genre3' }];
+    res.render('index', { books });
 };
